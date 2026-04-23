45c2b925ec2cd7a8faa526ff21135bf64259363e
shivamThakurXcode <shivam.thakur.official40@gmail.com>
2026-04-23 12:21:01 +0530

    push codde

---
+++ components/links.php
@@ -0,0 +1,33 @@
+ <!-- ========== Favicon ========== -->
+    <link
+      rel="shortcut icon"
+      href="assets/images/favicon.png"
+      type="image/x-icon"
+    />
+
+    <!-- ========== Google Fonts ========== -->
+    <link
+      href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&amp;display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&amp;display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- ========== Huge Icons CSS ========== -->
+    <link
+      rel="stylesheet"
+      href="assets/css/vendor/hugeicons/hgi-stroke-rounded.css"
+    />
+    <!-- ========== Nice Select CSS ========== -->
+    <link rel="stylesheet" href="assets/css/vendor/nice-select.css" />
+
+    <!-- ========== Slick CSS ========== -->
+    <link rel="stylesheet" href="assets/css/vendor/slick.min.css" />
+
+    <!-- ========== Animate CSS ========== -->
+    <link rel="stylesheet" href="assets/css/vendor/animate.min.css" />
+
+    <!-- ========== Custom CSS ========== -->
+    <link rel="stylesheet" href="assets/css/style.css" />
---
+++ components/preloader.php
@@ -0,0 +1,9 @@
+  <!-- Preloader Start -->
+    <div id="preloader" class="preloader">
+      <div class="pxl-loader-spinner">
+        <div class="pxl-loader-bounce1"></div>
+        <div class="pxl-loader-bounce2"></div>
+        <div class="pxl-loader-bounce3"></div>
+      </div>
+    </div>
+    <!-- Preloader End -->
