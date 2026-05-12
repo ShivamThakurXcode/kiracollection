42c4326bceb28bc4ee523ab737f0fb08720a9b6e
DigiconnUnite <shivam.thakur@digiconnunite.com>
2026-05-12 10:24:17 +0530

    change in the footer

--- components/preloader.php
+++ components/preloader.php
@@ -1,7 +1,7 @@
   <!-- Preloader Start -->
     <div id="preloader" class="preloader">
       <div class="pxl-loader-spinner">
-        <img src="images/header-logo.png" alt="Loading..." class="no-spin h-full w-full object-contain">
+        <img src="images/header-logo.png" alt="Loading..." class="no-spin h-full w-auto object-contain">
       </div>
     </div>
     <!-- Preloader End -->
