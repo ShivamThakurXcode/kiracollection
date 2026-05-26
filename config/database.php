233ad13fef80006638904bb95e802da35bc8f75c
DigiconnUnite <shivam.thakur@digiconnunite.com>
2026-05-26 09:08:02 +0530

    change the config

--- config/database.php
+++ config/database.php
@@ -5,11 +5,11 @@ $GLOBALS['db_name'] = 'kira';
 $GLOBALS['db_user'] = 'root';
 $GLOBALS['db_pass'] = 'root';
 
-// Also set locally for direct use
-$db_host = 'localhost';
-$db_name = 'kira';
-$db_user = 'root';
-$db_pass = 'root';
+// // Also set locally for direct use
+// $db_host = 'localhost';
+// $db_name = 'kira';
+// $db_user = 'root';
+// $db_pass = 'root';
 
 // Razorpay Payment Gateway - UPDATE these with your actual keys
 $razorpay_key_id = 'YOUR_RAZORPAY_KEY_ID'; // UPDATE: Razorpay Key ID from dashboard
